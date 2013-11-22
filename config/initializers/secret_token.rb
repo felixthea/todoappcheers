c9506b7b2cc054178f35256c1feae3246500ae06
Kimberly Narine & Felix Thea <kim+felix@appacademy.io>
2013-11-22 09:08:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '2d03a5d3e2e5edfb9f6c5664b7be65ee09b3d9c27133f247c5bc225a94b78ffb07e84a2d0c94cc380de4fa840202302b3fecf43488866ada6eb2ff6a021c6be1'
